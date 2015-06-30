0681a941670b192c4f2fd6fa49f3ed19b2f5aacf
Jip Eilbracht <jipeilbracht@gmail.com>
2015-06-30 20:13:53 +0200

    Clean up led functions
    Clear unused functions, put others in usage order

--- energy4/led_functions.ino
+++ energy4/led_functions.ino
@@ -13,19 +13,19 @@ void showleds()
         black();
         break;
       case 1:
-        fill_color(led_pos);
+        confetti(led_pos);
         break;
       case 2:
-        sinelon(led_pos);
+        trail(led_pos);
         break;
       case 3:
         ledset(rotary);
         break;
       case 4:
-        juggle(); //it needs to show this in order to show other animations?
+        juggle(); 
         break;
       case 5:
-        confetti(led_pos);
+        fill_color(led_pos);
         break;
     }
 
@@ -49,37 +49,36 @@ void black() //fill all the leds with black
   fill_solid(leds, NUM_LEDS, CHSV(0,0,0));
 }
 
-void fill_color(int hue) //fill all the leds with black
+void fill_color(int hue) //fill all the leds with a received colour
 {
   fill_solid(leds, NUM_LEDS, CHSV(hue,255,100));
 }
 
-void circular()
+//random colored speckles that blink, with adjustable amount of colours
+//TODO: make less speckles? slow down effect
+void confetti(int pos) 
 {
-  fadeToBlackBy( leds, NUM_LEDS, 1);
-  for (int i = 0; i < NUM_LEDS; i++){
-    leds[i] += CHSV(120,255,255);
-    if (i == NUM_LEDS) i = 0;
-  }
+  fadeToBlackBy( leds, NUM_LEDS, 5);
+  int pos2 = random16(NUM_LEDS);
+  leds[pos2] += CHSV( 0 + random8(pos), 150, 200); //randomize the colour a bit
 }
 
 
-void sinelon(int pos) //move a coloured dots with a trail, with max input value
+//move a coloured dots with a trail, with max input value
+void trail(int pos) 
 {
   pos = constrain(pos, 0, NUM_LEDS-1); //led_pos goes until 255, so constrain the value first
   fadeToBlackBy( leds, NUM_LEDS, 1);
-  leds[pos] += CHSV(110, 255, 255);
+  leds[pos] += CHSV(122, 255, 255); //same colour as the paint
 }
 
-
+//fill up the led strip from the top
 void ledset(int rotary) { //show a fixed point with a trail
-  //rotary = constrain(rotary, 0, NUM_LEDS-1); //led_pos goes until 255, so constrain the value first
-
   int hue = 64; //which colour? 0-255
-  int pos; //
+  int pos;
   
    pos = (rotary/10)-(rotary/40); //this keeps the last led at the top
-   pos = constrain(pos, 0, NUM_LEDS-1);
+   pos = constrain(pos, 0, NUM_LEDS-1); //keep it from overflowing
    
    fadeToBlackBy( leds, NUM_LEDS, 1);
    FastLED.clear();
@@ -87,22 +86,11 @@ void ledset(int rotary) { //show a fixed point with a trail
    //a loop to show all the leds <= pos
    for (int i = 0; i <= pos; i++) {
     leds[i] += CHSV(hue,255,255); //add the last led to show 
-   }
-    
-   
+   } 
 }
 
 
-void confetti(int pos) 
-{
-  // random colored speckles that blink
-  fadeToBlackBy( leds, NUM_LEDS, 5);
-  int pos2 = random16(NUM_LEDS);
-  leds[pos2] += CHSV( 0 + random8(pos), 200, 255); //randomize the colour a bit
-}
-
-
-//Back and forth filling up led strip
+//Back and forth filling up led strip with coloured dots
 void juggle() {
   fadeToBlackBy( leds, NUM_LEDS, 80);
   byte dothue = 0;
@@ -111,32 +99,3 @@ void juggle() {
     dothue += 32;
   }
 }
-
-
-void sampleled()
-{
-  // a colored dot sweeping back and forth, with fading trails
-  fadeToBlackBy( leds, NUM_LEDS, 1);
-  int pos = beatsin16(13,0,NUM_LEDS); //back and forth sine wave
-  leds[pos] += CHSV( 100, 255, 192);
-}
-
-void sampleled2()
-{
-  // a colored dot sweeping back and forth, with fading trails
-  fadeToBlackBy( leds, NUM_LEDS, 1);
-  int pos = beat8(120); //back and forth sine wave
-  leds[2] += CHSV( 100, 255, pos);
-}
-
-
-
-//void bpm() //slow coloured sprinkles
-//{
-//  uint8_t BeatsPerMinute = 12;
-//  CRGBPalette16 palette = PartyColors_p;
-//  uint8_t beat = beatsin8( BeatsPerMinute, 64, 255);
-//  for( int i = 0; i < NUM_LEDS; i++) { //9948
-//    leds[i] = ColorFromPalette(palette, 255, 255);
-//  }
-//}
